f510a773d30937a1b835e44eb59010eb856ae3e6
Jason Keeney <jason.keeney@gmail.com>
2016-08-19 19:14:50 +0800

    finished 05

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,26 @@
 class Book
-# write your code here
+
+  def title= book_name
+    @title = book_name
+    title
+  end
+
+  def title
+    words = @title.split(' ')
+    capitalwords=[]
+    littlewords = ['a', 'the', 'and', 'over', 'in', 'of', 'an']
+    words.each do |word|
+      if littlewords.include?(word)
+        if capitalwords.length == 0
+          capitalwords.push word.capitalize
+        else
+          capitalwords.push word
+        end
+      else
+        capitalwords.push word.capitalize
+      end
+    end
+    @title = capitalwords.join(' ')
+  end
+
 end
